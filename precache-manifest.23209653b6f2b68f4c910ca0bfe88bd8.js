1cc9e9171beefd9acc2db7ee5e0b981864f77caf
Edgar0995 <edgar.abgaryan15@gmail.com>
2023-05-10 19:02:23 +0200

    Deploy React app to GitHub Pages

---
+++ precache-manifest.23209653b6f2b68f4c910ca0bfe88bd8.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "47ef8f6555ccb958aaf3",
+    "url": "/edabgaryan/portfolio/static/css/main.9f4bebe3.chunk.css"
+  },
+  {
+    "revision": "47ef8f6555ccb958aaf3",
+    "url": "/edabgaryan/portfolio/static/js/main.47ef8f65.chunk.js"
+  },
+  {
+    "revision": "d7a0793f022bd00d59ab",
+    "url": "/edabgaryan/portfolio/static/js/1.d7a0793f.chunk.js"
+  },
+  {
+    "revision": "a8457d55adea8a17c771",
+    "url": "/edabgaryan/portfolio/static/js/runtime~main.a8457d55.js"
+  },
+  {
+    "revision": "4c66d6c5de5b29dd323ba5cc4e6e786b",
+    "url": "/edabgaryan/portfolio/static/media/gmail.4c66d6c5.svg"
+  },
+  {
+    "revision": "414c652ba7e92b0d1ea7cf12aaf7cd86",
+    "url": "/edabgaryan/portfolio/static/media/github.414c652b.svg"
+  },
+  {
+    "revision": "bd3ed08a73b459acc41d6dfebf5a47dc",
+    "url": "/edabgaryan/portfolio/static/media/telegram.bd3ed08a.svg"
+  },
+  {
+    "revision": "7ff000a7b0098f32715849a88b91f91c",
+    "url": "/edabgaryan/portfolio/static/media/vk.7ff000a7.svg"
+  },
+  {
+    "revision": "0aa8078c912de036c15f3c28e5814565",
+    "url": "/edabgaryan/portfolio/index.html"
+  }
+];
